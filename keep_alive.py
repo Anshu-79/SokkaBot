67c7920c6bce32fa6bdb84f872e1883fd095ab27
Anshu79 <51070518+Anshu-79@users.noreply.github.com>
2022-03-21 15:27:37 +0000

    modify keep_alive

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from threading import Thread
+from os import environ, system
+from logging import getLogger
 
-app = Flask("")
+system("cls||clear")
 
+# Edit these to your liking...
+write_to_console = False
+text = "Hello! I'm SokkaBot, a Discord bot made with disnake."
 
-@app.route("/")
+if not write_to_console:
+    getLogger('werkzeug').disabled = True
+    environ['WERKZEUG_RUN_MAIN'] = 'true'
+
+app = Flask('')
+
+@app.route('/')
 def home():
-    return "Hello. I am alive!"
-
+    return text
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
-
+  app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():
     t = Thread(target=run)
